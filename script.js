00a29021fe387fb7f1b40c1240990898cf8d941a
Dzusicek <113290637+Dzusicek@users.noreply.github.com>
2023-09-21 21:45:50 +0200

    First big commit
    Some html
    Some Css
    No JS
    Lots of bs

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const resultsContainer = document.getElementById("search-results-container");
+let html = `<div class="film-container">
+<img src="movie.img" alt="Movie image not availiable">
+<h3>Movie title ⭐</h3>
+</div>
+`;
+
+var docWidth = document.documentElement.offsetWidth;
+
+[].forEach.call(document.querySelectorAll("*"), function (el) {
+  if (el.offsetWidth > docWidth) {
+    console.log(el);
+  }
+});
