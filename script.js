4a3e6b49d9e0a4ced7d29dae036a78c57e5b0e8b
Dzusicek <113290637+Dzusicek@users.noreply.github.com>
2023-10-06 15:24:48 +0200

    Final commit for this project
    took me a while but found the stuff that messed up some of my functions, truns out i dont know how to use the function includes and indexOf.
    But found the problem and solved it! Very happy about that!

--- script.js
+++ script.js
@@ -5,46 +5,30 @@ const inputDivContainer = document.getElementById("input-area");
 const searchInput = document.getElementById("search-input-el");
 const searchBtn = document.getElementById("search-btn-el");
 const watchlistPageLink = document.getElementById("watchlistPageLink");
+
 //links to used images
 let plusIcon = "./imgs/Add_to_watchlist_icon.png";
 let minusIcon = "./imgs/Remove_from_watchlist_icon.png";
 
-//onload functions
-window.onload = function () {
-  if (localStorage.getItem("Watchlist") === null) {
-    localStorage.setItem("Watchlist", "[]");
-    watchlistArray = JSON.parse(localStorage.getItem("Watchlist"));
-  }
-  renderFunction();
-};
-
 //global variables
 let searchResultsArray = [];
-let watchlistArray = JSON.parse(localStorage.getItem("Watchlist"));
+let watchlistArray = JSON.parse(localStorage.getItem("Watchlist")) || [];
 
 //event listeners
 document.addEventListener("click", (e) => {
   if (resultsContainer) {
-    console.log("this");
     manageClick(e, searchResultsArray);
   } else if (watchlistContainer) {
-    console.log("that");
     manageClick(e, watchlistArray);
   }
 });
 
-//decides what happennes when user clicks something
+//decides further action according to what the user clicks
 function manageClick(e, array) {
   if (e.target.id === searchBtn.id) {
-    console.log("1");
     resultsContainer.innerHTML = "";
     searchTitles(searchInput.value);
-  } else if (e.target.id.includes("tt") && !watchlistArray) {
-    console.log("2");
-    watchlistArray = [];
-    addToWatchlist(e.target.id, array);
   } else if (e.target.id.includes("tt")) {
-    console.log("3");
     addToWatchlist(e.target.id, array);
   }
 }
@@ -57,7 +41,7 @@ async function searchTitles(searchData) {
     `https://www.omdbapi.com/?s=${searchData}&?type=movie&apikey=284f2e6d`
   );
   const data = await response.json();
-  //individualy searches first 4 movies for detailed information and than passes it on
+  //individualy searches first 4 movies for detailed information and than passes it on (is currently set to length of 4 to shorten loading time)
   for (let i = 0; i < 4 /*data.Search.length*/; i++) {
     const searchTitle = await fetch(
       `https://www.omdbapi.com/?t=${data.Search[i].Title}&apikey=284f2e6d`
@@ -69,15 +53,16 @@ async function searchTitles(searchData) {
 }
 
 //creates html and combines it with data input from APIs and returns it
-function createHtml(dataArray, icon) {
+function createHtml(dataArray) {
   let generatedHtml = [];
   for (const result of dataArray) {
-    if (watchlistArray.includes(result)) {
+    if (watchlistArray.some((e) => e.imdbID === result.imdbID)) {
       icon = minusIcon;
     } else {
       icon = plusIcon;
     }
-    generatedHtml.push(`<div class="film-container">
+    generatedHtml.push(`
+    <div class="film-container">
     <img class="film-poster" src="${result.Poster}" />
                 <div>
               <div class="film-title-container">
@@ -89,7 +74,7 @@ function createHtml(dataArray, icon) {
                 <p>${result.Runtime}</p>
                 <p>${result.Genre}</p>
                 <button type="button" id="${result.imdbID}" class="watchlist-btn-container">
-                  <img src="${icon}" id="${result.imdbID}btn">
+                  <img src="${icon}" id="${result.imdbID}">
                   Watchlist
                   </button>
               </div>
@@ -107,19 +92,18 @@ function createHtml(dataArray, icon) {
 
 //function filters and if needed pushes item in watchlist array
 function addToWatchlist(targetId, filteredArray) {
-  //variable providing information if item is already in watchlist
-  console.log(watchlistArray);
+  //not contained-variable providing information if item is already in watchlist
   let notContained = watchlistArray.every((listItem) => {
     return listItem.imdbID !== targetId;
   });
-  console.log(notContained);
   for (item of filteredArray) {
     if (targetId === item.imdbID && notContained) {
       watchlistArray.push(item);
-      console.log("A");
     } else if (targetId === item.imdbID && !notContained) {
-      watchlistArray.splice(watchlistArray.indexOf(item), 1);
-      console.log("B");
+      watchlistArray.splice(
+        watchlistArray.findIndex((e) => e.imdbID == item.imdbID),
+        1
+      );
     }
     localStorage.setItem("Watchlist", JSON.stringify(watchlistArray));
   }
@@ -128,10 +112,8 @@ function addToWatchlist(targetId, filteredArray) {
 
 //This function renders generated content based on what html elements exist
 function renderFunction() {
-  if (watchlistArray === null) {
-    localStorage.setItem("Watchlist", "[]");
-  } else if (watchlistContainer && watchlistArray.length) {
-    watchlistContainer.innerHTML = createHtml(watchlistArray, minusIcon);
+  if (watchlistContainer) {
+    watchlistContainer.innerHTML = createHtml(watchlistArray);
     inputDivContainer.style.display = "none";
   } else if (watchlistContainer && watchlistArray.length == 0) {
     watchlistContainer.innerHTML = `        <h2 id="placeholderImg">Your watchlist is looking a little empty...</h2>
@@ -141,7 +123,7 @@ function renderFunction() {
     inputDivContainer.style.display = "none";
   } else if (resultsContainer && searchResultsArray.length) {
     inputDivContainer.style.display = "visible";
-    resultsContainer.innerHTML = createHtml(searchResultsArray, plusIcon);
+    resultsContainer.innerHTML = createHtml(searchResultsArray);
   } else if (resultsContainer) {
     resultsContainer.innerHTML = `        <img
           id="placeholderImg"
@@ -151,3 +133,4 @@ function renderFunction() {
         <h3>Start Exploring!</h3>`;
   }
 }
+renderFunction();
